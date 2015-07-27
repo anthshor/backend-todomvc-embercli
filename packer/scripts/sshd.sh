e2823a8420c2fca33720cf7d1f2a5f179b3b27d9
Anthony Shorter <anthshor@gmail.com>
2015-07-27 14:59:22 +1200

    Removed packer

--- packer/scripts/sshd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-sudo apt-get install -y ssh openssh-server
-sudo update-rc.d ssh defaults
-
-echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
