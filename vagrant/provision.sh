b0606e9dbbe5eeac013c9b8785c04486cc80918e
Anthony <anthshor@gmail.com>
2015-07-29 11:29:09 +1200

    Replaced ember-app1 for todomvc-embercli

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,38 +4,30 @@
 [ -f /vagrant/proxy.env ] && source /vagrant/proxy.env
 
 pushd /home/vagrant
-if [ ! -d helloapp ]; then
-  sudo ember new helloapp
-  sudo chown -R vagrant: .config helloapp
+if [ ! -d todomvc-embercli ]; then
+  sudo ember new todomvc-embercli
+  sudo chown -R vagrant: .config todomvc-embercli
   [ -f /tmp/millis-test-sync ] && sudo chown vagrant: /tmp/millis-test-sync
 fi
 
 DATE="`date +%y%m%d%H%M`"
 
-if [ -d helloapp ]; then 
-  sudo chown -R vagrant: .config helloapp /tmp/millis-test-sync
-  pushd helloapp
-  if [ -d app ]; then 
-    pushd app
-    git remote -v | grep 'https://github.com/anthshor/emberjs-app1.git'
-    if [ $? -eq 0 ]; then
-      git pull
-      if [ $? -ne 0 ]; then
-        echo "git pull failed"
-        echo "review manually and fix the error"
-        exit 2
-      fi
-    else
-      popd
-      mv app app.${DATE}
-      git clone https://github.com/anthshor/emberjs-app1.git app
+if [ -d todomvc-embercli ]; then 
+  sudo chown -R vagrant: .config todomvc-embercli /tmp/millis-test-sync
+  git remote -v | grep 'https://github.com/anthshor/todomvc-embercli.git'
+  if [ $? -eq 0 ]; then
+    git pull
+    if [ $? -ne 0 ]; then
+       echo "git pull failed"
+       echo "review manually and fix the error"
+       exit 2
     fi
   else
-    git clone https://github.com/anthshor/emberjs-app1.git app
+    git clone https://github.com/anthshor/todomvc-embercli.git app
   fi
   [ -f /tmp/millis-test-sync ] && sudo chown vagrant: /tmp/millis-test-sync
   ember server
 else
-  echo "folder helloapp not found!!"
+  echo "folder todomvc-embercli not found!!"
   exit 1
 fi
